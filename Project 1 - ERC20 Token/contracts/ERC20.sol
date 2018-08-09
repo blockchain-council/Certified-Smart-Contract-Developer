ca43986ab423852d439d8afde5280ed27f823a81
Rohendra singh <rohendrasingh@gmail.com>
2018-08-09 13:20:51 +0530

    updated

--- Project 1 - ERC20 Token/contracts/ERC20.sol
+++ Project 1 - ERC20 Token/contracts/ERC20.sol
@@ -1,68 +1,68 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
 
 contract admined {
-	address public admin;
+    address public admin;
 
-	function admined() public {
-		admin = msg.sender;
-	}
+    constructor() public {
+        admin = msg.sender;
+    }
 
-	modifier onlyAdmin(){
-		require(msg.sender == admin) ;
-		_;
-	}
+    modifier onlyAdmin(){
+        require(msg.sender == admin, "Sender not authorized.");
+        _;
+    }
 
-	function transferAdminship(address newAdmin) onlyAdmin public  {
-		admin = newAdmin;
-	}
+    function transferAdminship(address newAdmin) public onlyAdmin {
+        admin = newAdmin;
+    }
 
 }
 
 contract TCoin {
 
-	mapping (address => uint256) public balanceOf;
-	mapping (address => mapping (address => uint256)) public allowance;
-	// balanceOf[address] = 5;
-	string public standard = "TCoin v1.0";
-	string public name;
-	string public symbol;
-	uint8 public decimals; 
-	uint256 public totalSupply;
-	event Transfer(address indexed from, address indexed to, uint256 value);
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+    // balanceOf[address] = 5;
+    string public standard = "TCoin v1.0";
+    string public name;
+    string public symbol;
+    uint8 public decimals; 
+    uint256 public totalSupply;
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
 
-	function TCoin(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) public {
-		balanceOf[msg.sender] = initialSupply;
-		totalSupply = initialSupply;
-		decimals = decimalUnits;
-		symbol = tokenSymbol;
-		name = tokenName;
-	}
+    constructor(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) public {
+        balanceOf[msg.sender] = initialSupply;
+        totalSupply = initialSupply;
+        decimals = decimalUnits;
+        symbol = tokenSymbol;
+        name = tokenName;
+    }
 
-	function transfer(address _to, uint256 _value) public {
-		require(balanceOf[msg.sender] > _value) ;
-		require(balanceOf[_to] + _value > balanceOf[_to]) ;
-		//if(admin)
+    function transfer(address _to, uint256 _value) public {
+        require(balanceOf[msg.sender] > _value, "Not enough balance");
+        require(balanceOf[_to] + _value > balanceOf[_to], "Integer Overflow");
+        //if(admin)
 
-		balanceOf[msg.sender] -= _value;
-		balanceOf[_to] += _value;
-		Transfer(msg.sender, _to, _value);
-	}
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+    }
 
-	function approve(address _spender, uint256 _value) public returns (bool success){
-		allowance[msg.sender][_spender] = _value;
-		return true;
-	}
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
 
-	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
-		require(balanceOf[_from] > _value) ;
-		require(balanceOf[_to] + _value > balanceOf[_to]) ;
-		require(_value < allowance[_from][msg.sender]) ;
-		balanceOf[_from] -= _value;
-		balanceOf[_to] += _value;
-		allowance[_from][msg.sender] -= _value;
-		Transfer(_from, _to, _value);
-		return true;
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[_from] > _value, "Not enough balance");
+        require(balanceOf[_to] + _value > balanceOf[_to], "Integer Overflow");
+        require(_value < allowance[_from][msg.sender], "Not enough allowance");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
 
-	}
+    }
 }
